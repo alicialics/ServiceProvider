ecc834e9cfe38787bf2ee18d4fc769ee3f3a10db
zhuo li <aliciali666@gmail.com>
2017-09-29 09:51:43 -0700

    add an user object

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(){
     Users user1("zhuo", "li", 60);
-    Users user2("jack", "zhao", 100);
+    Users user2("andrew", "fearing", 100);
     cout << user1.getFirst() << " " << user1.getLast() << endl;
     return 0;
     
